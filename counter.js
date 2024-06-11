7763ea5af3c8867a4c5beed120a68261d18ebb08
Lachine <86872452+Lachine1@users.noreply.github.com>
2024-06-11 15:59:21 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,15 +1,23 @@
 const eventGoal = 10000000000;
 async function fetchProgress(){
-  // Reverse engineerd by lachine
-  var res = await fetch('https://corsproxy.io/?https%3A%2F%2Fbrawlstars.inbox.supercell.com%2Fdata%2Fen%2Fnews%2Fcontent.json', {
-    method: 'GET',
-    mode: 'no-cors',
-    cache: 'no-cache',
-    headers: {
-    'Cookie':  '_sp_id.e079=56605ca7-a112-4068-969d-7f17e8cd843c.1710701679.3.1718047527.1718028414.c006d5f7-b056-4ee5-89f8-41368e2fcab6.b35254d8-4240-45da-bc90-2c77823f0364.0722776e-de58-4e6a-a588-a21ec1ff74ef.1718046689028.9; _sp_ses.e079=*',
-    'Referer': 'https://brawlstars.inbox.supercell.com/',
-    }
-  });
+  var res = await fetch("https://corsproxy.io/?https%3A%2F%2Fbrawlstars.inbox.supercell.com%2Fdata%2Fen%2Fnews%2Fcontent.json", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+        "Accept": "*/*",
+        "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
+        "Alt-Used": "brawlstars.inbox.supercell.com",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Priority": "u=4",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"
+    },
+    "referrer": "https://brawlstars.inbox.supercell.com/",
+    "method": "GET",
+    "mode": "cors"
+});
   console.log(res.body)
   var json = await res.json();
   var progressPercent = json.entries.eventEntries[0].tracker.progress;

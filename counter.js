9de6791fbdbf29ef7571d7457475bca5c95a4233
Lachine <86872452+Lachine1@users.noreply.github.com>
2024-06-13 15:58:23 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,15 +1,14 @@
 const eventGoal = 10000000000;
 async function fetchProgress(){
-  var res = await fetch("https://brawlstars.inbox.supercell.com/data/en/news/content.json", {
+  var res = await fetch("https://corsproxy.io/?https%3A%2F%2Fbrawlstars.inbox.supercell.com%2Fdata%2Fen%2Fnews%2Fcontent.json", {
     "credentials": "include",
     "headers": {
         "Alt-Used": "brawlstars.inbox.supercell.com",
         "Pragma": "no-cache",
         "Cache-Control": "no-cache"
     },
-    "referrer": "https://brawlstars.inbox.supercell.com/",
     "method": "GET",
-    "mode": "cors"
+    "mode": "no-cors"
 });
   console.log(res.body)
   var json = await res.json();
